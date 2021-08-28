7e64efdbca7644d1ac3de42051f4dca89bd188a0
PaulWeb30 <82338562+PaulWeb30@users.noreply.github.com>
2021-08-28 22:29:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const headerBurger = document.querySelector('.header__burger');
+const headerNav = document.querySelector('.header__navigation');
+const body = document.querySelector('body');
+headerBurger.addEventListener('click', function() {
+  this.classList.toggle('active');
+  headerNav.classList.toggle('active');
+  body.classList.toggle('lock');
+})
